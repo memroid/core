7068dc87995df18d02741e849303e075926dde13
Kirill Bronovitski <kirillbronovitski@gmail.com>
2024-12-13 18:10:03 +0100

    MMCR-1: add readFile method to read from dictionary and add cards to cardsCollection list

--- memroid/src/main/java/id/memro/Card.java
+++ memroid/src/main/java/id/memro/Card.java
@@ -3,4 +3,9 @@ package id.memro;
 public class Card {
     String sourceWord;
     String targetWord;
+
+    public Card(String sourceWord, String targetWord) {
+        this.sourceWord = sourceWord;
+        this.targetWord = targetWord;
+    }
 }
