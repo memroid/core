1eed88da9b516025022e0f003a6f3ec070ade4be
KirillBronovitski <95771264+KirillBronovitski@users.noreply.github.com>
2024-12-16 14:51:12 +0100

    Revert "Mmcr 1 core game ops"

--- id.memro.core/src/main/java/id/memro/Main.java
+++ id.memro.core/src/main/java/id/memro/Main.java
@@ -1,10 +1,7 @@
 package id.memro;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Game game1 = new Game();
-        game1.runGame(4);
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
     }
 }
